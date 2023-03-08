fd31d19e67742928998ee57862a9863c93d72b11
Noelí Rodríguez <noe.rmorales98@gmail.com>
2023-03-08 14:42:45 -0500

    final

--- IDYGS81-NoeliRodriguez/Controllers/LoginController.cs
+++ IDYGS81-NoeliRodriguez/Controllers/LoginController.cs
@@ -1,17 +1,26 @@
 ﻿using IDYGS81_NoeliRodriguez.Context;
+using IDYGS81_NoeliRodriguez.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Data;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace IDYGS81_NoeliRodriguez.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IConfiguration _configuration;
         private readonly ApplicationDbContext _context;
 
-        public LoginController(ApplicationDbContext context)
+        public LoginController(ApplicationDbContext context, IConfiguration configuration)
         {
-            _context = context; 
+            _context = context;
+            _configuration = configuration;
         }
 
 
@@ -20,18 +29,74 @@ namespace IDYGS81_NoeliRodriguez.Controllers
             return View();
         }
 
-
         [HttpPost]
-        public JsonResult Login(string user, string pass)
+        public async Task<IActionResult> Verificar(Usuario request)
         {
-            var response = _context.Usuarios.Where(x=> x.Nombre == user && x.Password == pass).ToList();
-            Console.WriteLine(response);
-
-            if (response.Count > 0)
+            try
             {
-                return Json(new {Succes =  true, admin =true});
+                var connectionString = _configuration.GetConnectionString("DefaultConnection");
+                using var connection = new SqlConnection(connectionString);
+                using var command = new SqlCommand("VerificarUsuario", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@correo", request.Correo);
+                command.Parameters.AddWithValue("@contrasena", GetHash(request.Password));
+
+                var resultado = new SqlParameter("@resultado", SqlDbType.Int);
+                resultado.Direction = ParameterDirection.Output;
+                command.Parameters.Add(resultado);
+                connection.Open();
+                command.ExecuteNonQuery();
+                var esValido = (int)resultado.Value == 1;
+                connection.Close();
+                if(esValido)
+                {
+                    using var command2 = new SqlCommand("TipoRol", connection);
+                    command2.CommandType = CommandType.StoredProcedure;
+                    command2.Parameters.AddWithValue("@correo", request.Correo);
+                    command2.Parameters.AddWithValue("@contrasena", GetHash(request.Password));
+                    var tipoRol = new SqlParameter("@resultado", SqlDbType.Int);
+                    tipoRol.Direction = ParameterDirection.Output;
+                    command2.Parameters.Add(tipoRol);
+                    connection.Open();
+                    command2.ExecuteNonQuery();
+                    int userRole = Convert.ToInt32(command2.Parameters["@resultado"].Value);
+                    connection.Close();
+                    if (userRole == 1)
+                    {
+                        return RedirectToAction("Index", "User");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index","Public");
+                    }
+                }
+                return RedirectToAction("Index");
+
+
             }
-            return Json(new { Succes = true, admin = true });
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+        public static string GetHash(string input)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // Convierte la entrada en bytes y genera el hash
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+                // Convierte el hash en una cadena
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
---
+++ IDYGS81-NoeliRodriguez/Controllers/PublicController.cs
@@ -0,0 +1,33 @@
+﻿using IDYGS81_NoeliRodriguez.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace IDYGS81_NoeliRodriguez.Controllers
+{
+    public class PublicController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PublicController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var response = _context.Usuarios.Include(x => x.Roles).ToList();
+            return View(response);
+   
+        }
+        public IActionResult Servicio()
+        {
+            var response = _context.Servicios.ToList();
+            return View(response);
+        }
+
+        public IActionResult Roles()
+        {
+            var response = _context.Roles.ToList();
+            return View(response);
+        }
+    }
+}
